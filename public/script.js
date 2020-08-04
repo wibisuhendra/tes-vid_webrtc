6e059f4a9122da1fda807bae7edc60b8afbbddc3
Wibi Suhendra <wibisuhendra@gmail.com>
2020-08-04 17:13:22 +0700

    peer port

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined,{
     host:'/',
-    port: '3001'
+    port: '4000'
 })
 const myVid = document.createElement('video')
 myVid.muted = true
